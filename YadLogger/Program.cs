1a3c4aae57d63e71298112f710e6aa751ad68b7f
Alireza Khorshidi <alikro2000@gmail.com>
2020-08-18 15:11:21 +0430

    📢 Initial Commit

---
+++ YadLogger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YadLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
